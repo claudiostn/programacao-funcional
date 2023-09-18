a0e0381b2fa774bfcbeed493655876211c2fc691
claudiostn <claudiostn26@gmail.com>
2023-09-18 18:01:01 -0300

    Definr função para calcular a media de tres nums

--- lab02.hs
+++ lab02.hs
@@ -5,3 +5,13 @@ ouExclusivo x y = (x || y) && not (x && y)
 
 ehMinusculo :: Char -> Bool
 ehMinusculo ch = ('a' <= ch) && (ch <= 'z')
+
+-- show converte valor para string
+-- read converte string pra valor
+-- ++ concatenacao
+
+mediaTres :: Float -> Float -> Float -> Float
+mediaTres a b c = (a + b + c)/3
+
+-- fromIntegral transforma int em float ou double
+
