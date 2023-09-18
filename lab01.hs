b82c55180234c1a75dde05a8082c2bc088715bfc
claudiostn <claudiostn26@gmail.com>
2023-09-18 17:24:04 -0300

    Definindo duas novas funções

--- lab01.hs
+++ lab01.hs
@@ -1,3 +1,11 @@
 dobro :: Int -> Int
 dobro x = 2*x
 
+soma :: Int -> Int -> Int 
+soma num1 num2 = num1 + num2
+
+ehMenor :: Int -> Int -> Bool
+ehMenor num1 num2 = num1 < num2 
+
+ehPar :: Int -> Bool
+ehPar num = (num `mod` 2) == 0
