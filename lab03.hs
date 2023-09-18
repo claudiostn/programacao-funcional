1ee0294590e984c0ac7114105c4a8844bc432b33
claudiostn <claudiostn26@gmail.com>
2023-09-18 19:04:05 -0300

    Adiconar os códigos dos exercicios

--- lab03.hs
+++ lab03.hs
@@ -6,3 +6,23 @@ infixl 7 &&&
 a &&& b
     | a >= b = a
     | otherwise = b
+
+infixl 7 ???
+(???) :: Int -> Int -> Int
+a ??? b = 2 * (a+b)
+
+infixl 7 @@@
+(@@@) :: Int -> Int -> Int
+a @@@ b = (a*b) ^ 2
+
+infixl 7 $$$
+($$$) :: Bool -> Bool -> Bool
+x $$$ y = (x || y) && not (x && y)
+paraRad :: Float -> Float
+paraRad graus = graus * (pi / 180)
+
+senGraus :: Float -> Float
+senGraus graus = sin (paraRad graus)
+
+cosGraus :: Float -> Float
+cosGraus graus = cos (paraRad graus)
