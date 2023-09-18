3f62188abd3d679b505c85bf49a02f888fd6f6ed
claudiostn <claudiostn26@gmail.com>
2023-09-18 17:26:02 -0300

    Definir uma funcao para saber se 3 nums são iguais

--- lab01.hs
+++ lab01.hs
@@ -9,3 +9,6 @@ ehMenor num1 num2 = num1 < num2
 
 ehPar :: Int -> Bool
 ehPar num = (num `mod` 2) == 0
+
+saoIguais :: Int -> Int -> Int -> Bool
+saoIguais num1 num2 num3 = (num1 == num2) && (num2 == num3)
